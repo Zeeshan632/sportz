4ba61cc42e69312d44266cca939fe820746fc9e1
Zeeshan Ali <zeeshanali632@Zeeshans-MacBook-Air.local>
2026-02-21 11:00:44 +0500

    implement arcjet security

--- src/ws/server.js
+++ src/ws/server.js
@@ -1,4 +1,5 @@
 import { WebSocket, WebSocketServer } from "ws";
+import { wsArcjet } from "../arcjet.js";
 
 function sendJson(socket, payload) {
   if (socket.readyState !== WebSocket.OPEN) return;
@@ -8,7 +9,7 @@ function sendJson(socket, payload) {
 
 function broadcast(wss, payload) {
   for (let client of wss.clients) {
-    if (client.readyState !== WebSocket.OPEN) return;
+    if (client.readyState !== WebSocket.OPEN) continue;
     client.send(JSON.stringify(payload));
   }
 }
@@ -21,12 +22,40 @@ export function attachWebsocketServer(server) {
     maxPayload: 1024 * 1024
   });
 
-  wss.on('connection', (socket) => {
-    sendJson(socket, {type: 'welcome'})
+  wss.on('connection', async(socket, req) => {
+    if(wsArcjet){
+      try {
+        const decision = await wsArcjet.protect(req)
+        if(decision.isDenied()){
+          const code = decision.reason.isRateLimit() ? 1013 : 1008;
+          const reason = decision.reason.isRateLimit() ? 'Rate limit exceeded' : 'Access denied';
+          socket.close(code, reason);
+          return;
+        }
+      }catch(err){
+        console.error('Error during wsArcjet handshake:', err);
+        socket.close(1011, 'Server security error');
+        return
+      }
+    }
+    
+    socket.isAlive = true;
+    socket.on('pong', () => {socket.isAlive = true})
 
+    sendJson(socket, {type: 'welcome'})
     socket.on('error', console.error)
   })
 
+  const interval = setInterval(() => {
+    wss.clients.forEach((socket) => {
+      if(socket.isAlive === false) return socket.terminate();
+      socket.isAlive = false;
+      socket.ping()
+    })
+  }, 30000)
+
+  wss.on('close', () => {clearInterval(interval)})
+  
   function broadcastMatchCreated(match){
     broadcast(wss, {type: 'match_created', data: match})
   }
